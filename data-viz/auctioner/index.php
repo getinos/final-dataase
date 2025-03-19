06c7f5fdf38c44b092b05a8dd74391ea805772c0
getinos <sujaydas0325@gmail.com>
2025-03-19 16:57:37 +0530

    final commit

---
+++ data-viz/auctioner/index.php
@@ -0,0 +1,19 @@
+<?php
+    // include './Backend/session_check.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="2">
+        <title>IPL Bidding Dashboard</title>
+        <link rel="stylesheet" href="style/styles1.css">
+    </head>
+    <body>
+    <div class="current-player">
+            
+            <?php include 'auctioner_details.php'; ?>
+
+        </div>
